6d96c33cf884752a842d984e4b461da88c884a6f
edzyl <edzyl_gu@compal.com>
2020-05-14 11:13:56 +0800

    [Modify] Path & date

--- deleteoldfile.py
+++ deleteoldfile.py
@@ -1,12 +1,18 @@
 import os
 import datetime
 
+# scanned paths
+Paths = '/home/edzyl/oldlog'
+# delete days
+Dday = 15
+
+
 def shouldkeep(file):
     if datetime.datetime.fromtimestamp( os.path.getmtime(file) ) > \
-        datetime.datetime.now() - datetime.timedelta(15):
+        datetime.datetime.now() - datetime.timedelta(Dday):
         return True
-for i in os.walk('/home/stevenli/control_center/share/web_control_center/data/debuglog/'):
+for i in os.walk(Paths):
      for j in i[2]:
         if not shouldkeep(os.path.join(i[0],j)):
             print os.path.join(i[0],j)
-            #os.remove( os.path.join(i[0],j) )
+            os.remove( os.path.join(i[0],j) )
